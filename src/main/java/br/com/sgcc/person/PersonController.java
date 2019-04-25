0775b63fe33200cb58265297b1c76dd0d94050f4
LucasFerrazSilva <lucasferrazsilva@hotmail.com>
2019-04-25 20:56:10 -0300

    Edit and remove functions added

--- src/main/java/br/com/sgcc/person/PersonController.java
+++ src/main/java/br/com/sgcc/person/PersonController.java
@@ -1,14 +1,18 @@
 package br.com.sgcc.person;
 
+import static java.lang.Integer.parseInt;
 import static org.springframework.data.domain.PageRequest.of;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,8 +32,16 @@ public class PersonController {
 		return "person/list";
 	}
 	
-	@GetMapping("/form")
-	public String form() {
+	@GetMapping(value = {"/form", "/form/{id}"})
+	public String form(@PathVariable Optional<String> id, Model model) {
+		Person obj = new Person();
+		
+		if(id.isPresent()) {
+			obj = repository.findById(parseInt(id.get())).get();
+		}
+		
+		model.addAttribute("obj", obj);
+		
 		return "person/form";
 	}
 	
@@ -38,6 +50,13 @@ public class PersonController {
 		System.out.println(person);
 		
 		repository.save(person);
+		
+		return "redirect:/person";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable String id) {
+		repository.deleteById(parseInt(id));
 		
 		return "redirect:/person";
 	}
